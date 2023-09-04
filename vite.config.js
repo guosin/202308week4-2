1fe9f5fad5a55cc5012c774fb18d245467564802
ksin <kbs@gmail.com>
2023-09-04 17:05:10 +0800

    hover修正完成

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/202308week4/',
+  base: '/202308week4-2/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
